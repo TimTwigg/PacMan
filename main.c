82b1afecb74b56d349b3bab173dae35102be7eb1
TimTwigg <63125914+TimTwigg@users.noreply.github.com>
2021-07-20 11:29:04 -0700

    Update main.c
    Added documentation
    Fixed ghost movement
    Fixed printing multiple ghosts in same cell
    Added random ghost turning
    Added turnGhost() function to manage turning ghosts
    Updated exitGame() to turn the map red, pause, and then exit
    Hid the cursor to get rid of the cursor flashing while updating the map

--- main.c
+++ main.c
@@ -89,6 +89,7 @@ Ghost ghosts[4] = { 0 };
 HANDLE hConsole;
 PacMan player;
 int mapSize;
+Color wallColor;
 void print(char* s, Color c);
 int coinFlip(void);
 void addOther();
@@ -97,7 +98,8 @@ void printMap(void);
 void updateFrame(void);
 void manageMove(void);
 int move(void);
-void moveGhosts(void);
+int moveGhosts(void);
+Direction turnGhost(Direction dir);
 int writeAnsi(PCWSTR sequence);
 void up(void);
 void down(void);
@@ -107,9 +109,12 @@ void pause(void);
 int checkMap(void);
 void exitGame(void);
 int getIndex(Direction direction);
+void hideCursor(void);
 
 
 void main(void) {
+	/*Intitial setup*/
+	hideCursor();
 	srand((unsigned int)time(NULL));
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	player.score = 0;
@@ -125,7 +130,9 @@ void main(void) {
 			g++;
 		}
 	}
+	wallColor = GRAY;
 
+	/*Start the game*/
 	puts("Welcome to PacMan!");
 	addOther();
 	printMap();
@@ -133,12 +140,14 @@ void main(void) {
 }
 
 void print(char* s, Color c) {
+	/*Print a char array in color*/
 	SetConsoleTextAttribute(hConsole, c);
 	printf("%s", s);
 	SetConsoleTextAttribute(hConsole, STD);
 }
 
 int coinFlip(void) {
+	/*Psuedo-random 0/1 generator*/
 	int n = rand();
 	if (n % 2 == 0) {
 		return 0;
@@ -149,7 +158,7 @@ int coinFlip(void) {
 }
 
 void addOther(void) {
-	/*Add secondary identifiers to map grid*/
+	/*Add secondary identifiers (*food) to map grid*/
 	for (int i = 0; i < mapSize; i++) {
 		for (int j = 0; j < mapSize; j++) {
 			if (mapGrid[i][j] == PATH && coinFlip() == 1 && coinFlip() == 1) {
@@ -160,6 +169,7 @@ void addOther(void) {
 }
 
 Ghost makeGhosts(int x, int y) {
+	/*Create a ghost object from the Ghost template*/
 	Ghost temp;
 	temp.x = x;
 	temp.y = y;
@@ -173,17 +183,23 @@ void printMap(void) {
 	for (int i = 0; i < mapSize; i++) {
 		for (int j = 0; j < mapSize; j++) {
 			int printed = FALSE;
-			if (player.x - 1 == j && player.y - 1 == i) {
-				printf("%s", dirSymbols[player.symbol]);
-				printed = TRUE;
-			}
+
+			/*Check for and print ghosts*/
 			for (int g = 0; g < 4; g++) {
 				Ghost gh = ghosts[g];
-				if (gh.x - 1 == j && gh.y - 1 == i) {
-					printf("%i", gh.y);
+				if (gh.x - 1 == j && gh.y - 1 == i && !printed) {
+					print("@", RED_LIGHT);
 					printed = TRUE;
 				}
 			}
+
+			/*Check for and print player symbol*/
+			if (player.x - 1 == j && player.y - 1 == i && !printed) {
+				print(dirSymbols[player.symbol], GREEN_LIGHT);
+				printed = TRUE;
+			}
+
+			/*If no player or ghost, print out walls, food, and empty path*/
 			if (!printed) {
 				int id = mapGrid[i][j];
 				switch (id) {
@@ -191,7 +207,7 @@ void printMap(void) {
 					printf(" ");
 					break;
 				case WALL:
-					print("+", GRAY);
+					print("+", wallColor);
 					break;
 				case FOOD:
 					print("*", CYAN);
@@ -205,12 +221,15 @@ void printMap(void) {
 }
 
 void updateFrame(void) {
-	writeAnsi(L"\033[2;1H");
+	/*Refresh the map*/
+	writeAnsi(L"\033[2;1H"); // Moves the cursor to the start of the second line on the screen
 	printMap();
 }
 
 void manageMove(void) {
+	/*Manage player movement, including receiving turn commands from player and calling the move() function to execute movement*/
 	while (1) {
+		/*Update player's direction and symbol if a key was pressed*/
 		if (_kbhit()) {
 			int key = _getch();
 			if (key == UP && player.direction != DOWN) {
@@ -233,23 +252,30 @@ void manageMove(void) {
 				return;
 			}
 			else {
-				continue;
+				continue; // TODO: add pause game functionality?
 			}
 		}
+
+		/*Move the player and ghosts, then pause*/
 		if (!move()) break;
-		moveGhosts();
+		if (!moveGhosts()) break;
 		pause();
 	}
 	exitGame();
 }
 
 int move(void) {
+	/*Move the player*/
 	updateFrame();
 	int nextBlock = -1;
 	writeAnsi(L"\033[16;1H");
+	
+	/*Save the player's old coords in case they are facing a wall and can't move*/
 	Coordinate old;
 	old.x = player.x;
 	old.y = player.y;
+
+	/*Update player's coords*/
 	switch (player.direction) {
 	case UP:
 		if (player.y > 1) {
@@ -273,18 +299,22 @@ int move(void) {
 		break;
 	}
 	nextBlock = mapGrid[player.y - 1][player.x - 1];
+
+	/*Restore old coords if the player is facing a wall*/
 	if (nextBlock == WALL) {
 		player.x = old.x;
 		player.y = old.y;
 		return TRUE;
 	}
 
+	/*Add score if player eats food*/
 	if (nextBlock == FOOD) {
 		player.score += 10;
 		mapGrid[player.y - 1][player.x - 1] = PATH;
 		if (checkMap()) addOther();
 	}
 
+	/*Check for player being killed by ghost (same coords on map)*/
 	for (int g = 0; g < 4; g++) {
 		Ghost gh = ghosts[g];
 		if (gh.x == player.x && gh.y == player.y) {
@@ -295,12 +325,17 @@ int move(void) {
 	return TRUE;
 }
 
-void moveGhosts(void) {
+int moveGhosts(void) {
+	/*Move the ghosts*/
 	for (int g = 0; g < 4; g++) {
 		Ghost gh = ghosts[g];
+		
+		/*Save the ghost's coords*/
 		Coordinate old;
 		old.x = gh.x;
 		old.y = gh.y;
+
+		/*Update ghost coords for movement*/
 		switch (gh.direction) {
 		case UP:
 			if (gh.y > 1) {
@@ -323,15 +358,34 @@ void moveGhosts(void) {
 			}
 			break;
 		}
+
+		/*randomly turn ghost*/
+		gh.direction = turnGhost(gh.direction);
+
+		/*Restore old coords if facing a wall*/
 		if (mapGrid[gh.y - 1][gh.x - 1] == WALL) {
 			gh.x = old.x;
 			gh.y = old.y;
-			int index = getIndex(gh.direction);
-			if (coinFlip()) index++;
-			else index += 3;
-			if (index > 3) index -= 4;
+		}
+		ghosts[g] = gh;
+
+		/*End game if ghost eats player*/
+		if (player.x == gh.x && player.y == gh.y) {
+			return FALSE;
 		}
 	}
+	return TRUE;
+}
+
+Direction turnGhost(Direction dir) {
+	/*Randomly choose a new direction for ghost*/
+	int index = getIndex(dir);
+	if (coinFlip()) {
+		if (coinFlip()) index++;
+		else index += 3;
+		if (index > 3) index -= 4;
+	}
+	return dirs[index];
 }
 
 int writeAnsi(PCWSTR sequence) {
@@ -373,6 +427,7 @@ void pause(void) {
 }
 
 int checkMap(void) {
+	/*Checks for remaining food on map and adds new food if necessary*/
 	for (int i = 0; i < mapSize; i++) {
 		for (int j = 0; j < mapSize; j++) {
 			if (mapGrid[i][j] == FOOD) {
@@ -384,15 +439,30 @@ int checkMap(void) {
 }
 
 void exitGame(void) {
+	wallColor = RED_DARK;
+	updateFrame();
+	Sleep(2000);
 	system("cls");
 	printf("Oh No You Lost!\nYour score was %i\n", player.score);
 }
 
+// TODO: save high score?
+
 int getIndex(Direction direction) {
+	/*Get the index of a direction in the dirs array*/
 	for (int g = 0; g < 4; g++) {
 		if (direction == dirs[g]) {
 			return g;
 		}
 	}
 	return -1;
+}
+
+void hideCursor(void) {
+	/*Hides the cursor. Copied from StackOverflow - https://stackoverflow.com/questions/30126490/how-to-hide-console-cursor-in-c */
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;
+	info.bVisible = FALSE;
+	SetConsoleCursorInfo(consoleHandle, &info);
 }
